4e72b925f0bbe9a099d1935d9599838c0cbfad36
Oscar <oscar.pena@deusto.es>
2013-03-04 11:43:28 +0100

    GoogleReverseCoder able to detect bounding box.

--- src/location/resolution/services/GoogleReverseCoder.java
+++ src/location/resolution/services/GoogleReverseCoder.java
@@ -14,7 +14,9 @@ import java.util.List;
 import java.util.Properties;
 
 import location.resolution.aux.Utils;
+import location.resolution.models.BoundingBox;
 import location.resolution.models.GeoPoint;
+import location.resolution.models.LocationDescriptor;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -54,11 +56,12 @@ public class GoogleReverseCoder {
 	}
 	
 	private GeoPoint fillGeoPoint(JSONObject jsonObject) {
-		GeoPoint geoPoint = new GeoPoint();		
+		GeoPoint geoPoint = new GeoPoint();
 		
 		try {
-			geoPoint.setLatitude(jsonObject.getDouble("lat"));
-			geoPoint.setLongitude(jsonObject.getDouble("lng"));
+			JSONObject locationJSONObject = jsonObject.getJSONObject("location");
+			geoPoint.setLatitude(locationJSONObject.getDouble("lat"));
+			geoPoint.setLongitude(locationJSONObject.getDouble("lng"));
 		}
 		catch (JSONException e) {
 			e.printStackTrace();
@@ -67,8 +70,27 @@ public class GoogleReverseCoder {
 		return geoPoint;
 	}
 	
-	public List<GeoPoint> searchPlace(String placename) {
-		List<GeoPoint> geoPoints = new ArrayList<GeoPoint>();
+	private BoundingBox fillBoundingBox(JSONObject jsonObject) {
+		BoundingBox boundingBox = new BoundingBox();
+		
+		try {
+			JSONObject viewportJSONObject = jsonObject.getJSONObject("viewport");
+			
+			JSONObject southwestJSONObject = viewportJSONObject.getJSONObject("southwest");
+			JSONObject northeastJSONObject = viewportJSONObject.getJSONObject("northeast");
+							
+			boundingBox.addGeoPoint(new GeoPoint(southwestJSONObject.getDouble("lat"), southwestJSONObject.getDouble("lng")));
+			boundingBox.addGeoPoint(new GeoPoint(northeastJSONObject.getDouble("lat"), northeastJSONObject.getDouble("lng")));
+		}
+		catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return boundingBox;
+	}
+	
+	public List<LocationDescriptor> searchPlace(String placename) {
+		List<LocationDescriptor> locationDescriptors = new ArrayList<LocationDescriptor>();
 		try {
 			InputStream is = new URL(formURL(placename)).openStream();
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
@@ -81,9 +103,12 @@ public class GoogleReverseCoder {
 	    	for(int i = 0; i < jsonArray.length(); i++) {
 	    		JSONObject jsonObject = jsonArray.getJSONObject(i);
 	    		
-	    		JSONObject latlngJSONObject = jsonObject.getJSONObject("geometry").getJSONObject("location");
+	    		JSONObject geometryJSONObject = jsonObject.getJSONObject("geometry");
 	    		
-	    		geoPoints.add(fillGeoPoint(latlngJSONObject));
+	    		GeoPoint geopoint = fillGeoPoint(geometryJSONObject);
+	    		BoundingBox boundingBox = fillBoundingBox(geometryJSONObject);
+	    		
+	    		locationDescriptors.add(new LocationDescriptor(geopoint, boundingBox));
 	    	}
 		}
 		catch (MalformedURLException e) {
@@ -96,6 +121,16 @@ public class GoogleReverseCoder {
 			e.printStackTrace();
 		}
 		
-		return geoPoints;
+		return locationDescriptors;
+	}
+	
+	public static void main(String[] args) {
+		GoogleReverseCoder grc = new GoogleReverseCoder();
+		
+		List<LocationDescriptor> lld = grc.searchPlace("vitoria-gasteiz");
+        
+		for(LocationDescriptor ld: lld) {
+			System.out.println(ld.toString());
+		}
 	}
 }
